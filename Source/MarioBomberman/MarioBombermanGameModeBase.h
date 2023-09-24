03a72d2a34af3af6920ddef548146b72d63a9b87
jaisharma06 <jai.sharma06@live.com>
2023-09-24 23:47:45 +0530

    initial commit

---
+++ Source/MarioBomberman/MarioBombermanGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarioBombermanGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARIOBOMBERMAN_API AMarioBombermanGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
